cb83314850aff1b5f3f2f4ed4a029235fb55d274
sarfarajansari <sarfarajansaridev@gmail.com>
2024-08-15 23:36:33 +0530

    updated api

--- src/apiClient/apiClient.js
+++ src/apiClient/apiClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://edutube-fastapi.onrender.com",
+  // baseURL: "https://edutube-fastapi.onrender.com",
+  baseURL:'https://edutubeapi.azurewebsites.net'
+  // baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
